9d4e1c7480218daea5aca4d013b435ea1bd4b296
drklee3 <dlee3@scu.edu>
2018-02-15 23:23:23 -0800

    add sushii logos and basic info

--- pages/about.jsx
+++ pages/about.jsx
@@ -10,7 +10,7 @@ const AboutPage = () => (
             </h1>
             <div className="content">
                 <p>sushii is written in <a href="https://www.rust-lang.org/" target="_blank">Rust</a> with <a href="https://github.com/zeyla/serenity/" target="_blank">serenity-rs</a>.</p>
-                <p>This site was made with <a href="https://www.gatsbyjs.org/" target="_blank">GatsbyJS</a> and <a href="https://bulma.io" target="_blank">Bulma</a>.</p>
+                <p>This site was made with <a href="https://github.com/zeit/next.js/" target="_blank">Next.js</a>, <a href="https://reactjs.org/" target="_blank">React</a>, and <a href="https://bulma.io" target="_blank">Bulma</a>.</p>
             </div>
         </div>
     </Layout>
--- pages/index.jsx
+++ pages/index.jsx
@@ -8,6 +8,7 @@ const IndexPage = () => (
       <section className="hero is-fullheight">
         <div className="hero-body">
           <div className="container has-text-centered">
+            <img src="/static/sushii.png" />
             <h1 className="title">
               sushii
             </h1>
