46537216fe63e94d62b8053ee5ec4820d59a8a1e
drklee3 <dlee3@scu.edu>
2018-02-28 22:35:22 -0800

    adjust wording for about

--- pages/about.jsx
+++ pages/about.jsx
@@ -8,8 +8,8 @@ const AboutPage = () => (
         About
       </h1>
       <div className='content'>
-        <p>sushii is written in <a href='https://www.rust-lang.org/' target='_blank' rel='noopener noreferrer'>Rust</a> with <a href='https://github.com/zeyla/serenity/' target='_blank' rel='noopener noreferrer'>serenity-rs</a>.</p>
-        <p>This site was made with <a href='https://github.com/koajs/koa' target='_blank' rel='noopener noreferrer'>Koa</a>, <a href='https://github.com/zeit/next.js/' target='_blank' rel='noopener noreferrer'>Next.js</a>, <a href='https://reactjs.org/' target='_blank' rel='noopener noreferrer'>React</a>, and <a href='https://bulma.io' target='_blank' rel='noopener noreferrer'>Bulma</a>.</p>
+        <p>sushii is written in <a href='https://www.rust-lang.org/' target='_blank' rel='noopener noreferrer'>Rust</a> with <a href='https://github.com/zeyla/serenity/' target='_blank' rel='noopener noreferrer'>serenity-rs</a> and <a href='https://www.postgresql.org/' target='_blank' rel='noopener noreferrer'>PostgreSQL</a>.</p>
+        <p>This site is made with <a href='https://github.com/koajs/koa' target='_blank' rel='noopener noreferrer'>Koa</a>, <a href='https://github.com/zeit/next.js/' target='_blank' rel='noopener noreferrer'>Next.js</a>, <a href='https://reactjs.org/' target='_blank' rel='noopener noreferrer'>React</a>, and <a href='https://bulma.io' target='_blank' rel='noopener noreferrer'>Bulma</a>.</p>
       </div>
     </div>
   </Layout>
