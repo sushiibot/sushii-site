de9339ffae21302aa9698de16408777e53fa2371
drklee3 <dlee3@scu.edu>
2018-03-03 16:48:40 -0800

    :fire: Removed example Koa routes

--- src/server.js
+++ src/server.js
@@ -35,16 +35,6 @@ app.prepare()
       })
     )
 
-    router.get('/a', async ctx => {
-      await app.render(ctx.req, ctx.res, '/b', ctx.query)
-      ctx.respond = false
-    })
-
-    router.get('/b', async ctx => {
-      await app.render(ctx.req, ctx.res, '/a', ctx.query)
-      ctx.respond = false
-    })
-
     router.get('*', async ctx => {
       await handle(ctx.req, ctx.res)
       ctx.respond = false
