766f33143fc7d9a6353a26e7fa8bd8f04059722f
drklee3 <dlee3@scu.edu>
2018-04-02 21:09:12 -0700

    Fix next-plugin dependencies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 // next.config.js
+const withCSS = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-  sassLoaderOptions: {
-    includePaths: ['styles/index.scss']
+const commonsChunkConfig = require('@zeit/next-css/commons-chunk-config')
+
+module.exports = withSass(withCSS({
+  webpack(config) {
+    config = commonsChunkConfig(config, /\.(sass|scss|css)$/)
+    return config
   }
-})
+}))
